2ba6f79cff2ca33470ad02822869eb44f01ffa3f
Hiren Bhavsar <bhavsar.hsb@gmail.com>
2019-09-17 12:26:05 -0400

    Some refactoring and added joke command
    + Joke Command
    + Refactored code to respond to only non bot messages that use the proper prefix

--- index.js
+++ index.js
@@ -12,13 +12,30 @@ client.once('ready', () => {
 	console.log('Ready!');
 });
 
-client.on('message', message =>{
-    // console.log(message.content)
+//Khepri Jokes
+var jokes = ["What did the iceberg say to the sun? You crack me up. Ha! Hahahahaha!","I lost the sun for a second, but then it dawned on me.","The sun enjoys reading, you know. Just so that it may get brighter.", "Why don't lobsters share their food? Because they're shellfish!", "What happened when the crustacean was late to work? She lobster job! Hahahahahaha!","I'm no feeder! Well okay, I'm a bottom-feeder, but I fight to win!"];
 
-    if(message.content.toLowerCase() === `${prefix}hello`){
+client.on('message', async message =>{
+
+    if(message.author.bot) return;
+    if(message.content.indexOf(prefix)!==0) return;
+    
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+
+    if(command === "hello"){
         message.channel.send('Greetings, friend!')
     }
+
+    if(command === "joke"){
+        var ran = getRndInteger(1,jokes.length);
+        message.channel.send(jokes[ran-1]);    
+    }
 })
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+  }
 
 // login to Discord with your app's token
 client.login(token);
